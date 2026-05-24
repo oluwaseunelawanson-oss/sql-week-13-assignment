81cfb6f873cf3a6d23d606858b6ffa5c3c35e7c7
oluwaseunelawanson-oss <oluwaseunelawanson@gmail.com>
2026-05-24 09:29:02 -1200

    Add files via upload

---
+++ SQL_WEEK13_LAWANSON-OLUWASEUN.sql
@@ -0,0 +1,10 @@
+SELECT *
+FROM EmployeeDemographics
+ORDER BY 4 DESC, 5 DESC
+
+
+--SELECT Gender, COUNT(Gender) As COUNTGender
+--FROM EmployeeDemographics
+--WHERE Age > 31
+--GROUP BY Gender
+--ORDER BY Gender
